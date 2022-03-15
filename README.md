e16107bb939e1f76ffb7c9a0825c5a8c23248254
cyrille-mktro <cyrille.kone@icloud.com>
2022-03-15 09:17:04 +0100

    utils & readme

---
+++ README.md
@@ -0,0 +1,12 @@
+This is the repo for the project of studying the paper <a href="https://arxiv.org/pdf/1602.02666.pdf"> A Variational Analysis of Stochastic Gradient Algorithms </a>
+#Setup  
+<code> 
+pip install -r requirements.txt
+</code>
+
+#Run 
+#TODO 
+<code> python3 main.py   </code>
+
+# Tasks 
+#TODO
---
+++ utils.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+"""
+PyCharm Editor
+Author @git cyrille-kone & geoffroyO
+"""
+import torch as th
+import torch.utils.data as thd
+import numpy as np
+from pathlib import Path
+from sklearn.datasets import fetch_covtype
+
+DATA_DIR = Path("./data/.covtype")
+DATA_DIR.mkdir(exist_ok=True)
+
+
+class ForestCoverType(thd.Dataset):
+    r'''Implement a custom dataloader'''
+
+    def __init__(self) -> None:
+        self.data, self.targets = fetch_covtype(data_home=DATA_DIR, return_X_y=True)
+
+    def __getitem__(self, idx) -> tuple:
+        data, target = self.data[idx], self.targets[idx]
+        return data, target
+
+    # override
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def random_split(self, lengths=None):
+        r'''Split the data set into parts '''
+        lengths = [0.8, 0.2] if lengths is None else lengths
+        assert sum(lengths) == 1, " Lengths should sum up to one"
+        lengths = (len(self.data) * th.tensor(lengths)).int()
+        if sum(lengths) != len(self.data):
+            lengths[0] += len(self.data) - sum(lengths)
+        return thd.random_split(self, lengths)
+
+
+if __name__ == "__main__":
+    print("Import Forest CoverType dataset")
+    fct = ForestCoverType()
+    fct_train, fct_val = fct.random_split()
+    fct_loader = thd.DataLoader(fct, batch_size=10)
