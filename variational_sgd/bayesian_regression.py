a41671900e1a05ffe0508d010a3f10eb50d536e0
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2022-03-21 20:00:14 +0100

    comment mcmc sampling

--- variational_sgd/bayesian_regression.py
+++ variational_sgd/bayesian_regression.py
@@ -5,25 +5,26 @@ import arviz as az
 
 
 def get_mcmc_sample(X, y, size=5):
+    """ MCMC sampling """ 
     prior_location = 0
     prior_scale = 1
     with pm.Model() as model:
-        # Prior sur l'intercept
+        # Prior on the interceptt
         alpha = pm.Normal('alpha', mu=prior_location, sigma=prior_scale)
 
-        # Prior sur les coefficients de la régression linéaire
+        # Prior on the coefficient 
         beta = pm.Normal('beta', mu=prior_location, sigma=prior_scale, shape=size)
 
-        # Prior sur l'écart type des résultats
+        # Prior on std
         log_sigma = pm.Normal('log_sigma', mu=prior_location, sigma=prior_scale)
 
-        # Régression linéaire
+        # linear model
         mu = alpha + pm.math.dot(X, beta)
 
-        # Vraissemblance
+        # likelihood
         likelihood = pm.Normal('likelihood', mu=mu, sigma=np.exp(log_sigma), observed=y)
 
-        # MCMC
+        # MCMC sampling
         trace = pm.sample(2000)
 
     return trace
