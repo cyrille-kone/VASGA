9d224fa89fa235cb814ef8aa7c638412e4031f82
geoffroyO <46093940+geoffroyO@users.noreply.github.com>
2022-03-21 20:00:56 +0100

    comment lyapunoc

--- variational_sgd/tools.py
+++ variational_sgd/tools.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def lyapunov(A, B, H, eps):
+    """ Returns the solution of the Lyapunov equation given HA sdp matrix """
     U, S, _ = np.linalg.svd(H @ A)
     sigma = np.zeros(A.shape)
     for i in range(len(A)):
